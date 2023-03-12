7dbf051cdd52b7d7a196507be5c1e8818c43111f
lyucean <lyucean@gmail.com>
2023-03-12 03:26:18 +0300

    js style

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -21,26 +21,27 @@ function Route () {
       $('#app').load('/home.html')
   }
 }
+
 $(window).on('hashchange', function () {
   Route()
 })
-Route();
+Route()
 
 // init page Choice
 function initChoice () {
   // подгрузим варианты форматов для сравнения
   $.getJSON('http://localhost/api/get/formats.json', function (data) {
 
-    let items_a = [], items_b = [], status = '', id_a, id_b;
+    let items_a = [], items_b = [], status = '', id_a, id_b
 
-    $.each(data.formats, function (key,item) {
+    $.each(data.formats, function (key, item) {
 
       id_a = 'a_' + item['name'] + '_' + key
       id_b = 'b_' + item['name'] + '_' + key
 
-      status = "";
-      if(item.hasOwnProperty('checked')){
-        status = "checked";
+      status = ''
+      if (item.hasOwnProperty('checked')) {
+        status = 'checked'
       }
 
       items_a.push('<div class="form-check">' +
@@ -63,7 +64,7 @@ function initChoice () {
   }).fail(function () {
     // спросим через ещё раз
     setTimeout(() => {
-      initChoice ();
-    }, 3000);
+      initChoice()
+    }, 3000)
   })
 }
