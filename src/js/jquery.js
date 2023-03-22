e3e574b61d1911221bc79a545786465cdbfa1d7d
lyucean <lyucean@gmail.com>
2023-03-23 01:23:02 +0300

    Докинул базовый плеер

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -16,10 +16,12 @@ function Route () {
       break
     case 'test':
       div_app.load('/test.html', function () {
-
         // проверка, что выбран формат
         if ($.urlGET('v1') && $.urlGET('v2')) {
           initTest()
+        } else {
+          // если что-то не так, отправим выбрать формат
+          window.location.href = '#choice'
         }
       })
       break
@@ -91,17 +93,68 @@ function RenderChoice (variant, formats) {
   $('#variant_' + variant).html(items_a.join(''))
 }
 
-function initTest(){
+let button_icon_play = '<i class="bi bi-play-fill"></i>',
+  button_icon_pause = '<i class="bi bi-pause-fill"></i>'
 
-// подгрузим тесты
-  $.getJSON('http://localhost/api/get/test.json', function (data) {
+function initTest () {
 
-    console.log(data)
+  let audio = document.createElement('audio')
+  // соберём аргументы для url
+  let arg_url = '?v1=' + $.urlGET('v1') + '&v2=' + $.urlGET('v2')
+
+  // подгрузим варианты теста
+  $.getJSON('http://localhost/api/get/test.json' + arg_url, function (data) {
+
+    // API вернёт ответ
+    // if (typeof data['tracks'] !== "undefined") {
+    //
+    // }else{
+    //   //ключа нет
+    //   console.log(1)
+    //
+    // }
+
+    $('#button_play_a').click(function () {
+      audioPlayer($(this), audio, data.tracks.urls.a)
+    })
+    $('#button_play_b').click(function () {
+      audioPlayer($(this), audio, data.tracks.urls.b)
+    })
+    $('#button_play_x').click(function () {
+      audioPlayer($(this), audio, data.tracks.urls.x)
+    })
+
+    // arr.indexOf(data.tracks) != -1
 
   }).fail(function () {
     // спросим через ещё раз
-    initTest(() => {
-      initChoice()
+    setTimeout(() => {
+      initTest()
     }, 3000)
   })
+}
+
+function audioPlayer (button, audio, data) {
+
+  // остановим воспроизведение
+  audio.pause()
+
+  // если мы кликаем по той же кнопке, просто сбросим её состояние на паузу
+  if (button.html() === button_icon_pause) {
+    button.html(button_icon_play)
+  } else {
+    // Если это не так, для начала сбросим состояние всех кнопок
+    $('.answer-options button.card-body-play').html(button_icon_play)
+    // Сменим ресурс и запустим проигрыватель
+    audio.setAttribute('src', data)
+    audio.play()
+    // сменим иконку на воспроизведение
+    button.html(button_icon_pause)
+  }
+
+  // по завершению, сбросим состояние кнопки
+  audio.addEventListener('ended', function() {
+    this.pause();
+    button.html(button_icon_play)
+  }, false);
 }
