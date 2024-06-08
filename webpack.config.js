a1feecdcb5c23d485c22b051a8b67c1d619bb900
lyucean <lyucean@gmail.com>
2024-06-09 02:49:41 +0300

    Сложим фронт в свою папку

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,23 @@ const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const webpack = require('webpack');
 let fs = require('fs');
 
-const base_template = path.join(__dirname, 'src', 'index.html'); // базовый шаблон
+const base_template = path.join(__dirname, 'frontend', 'index.html'); // базовый шаблон
 
 module.exports = {
   mode: 'development',
   entry: {
     main: {
-      import: path.join(__dirname, 'src/js', 'index.js'),
+      import: path.join(__dirname, 'frontend/js', 'index.js'),
       filename: '[name].[contenthash].index.js',
     },
     jquery: {
       dependOn: 'jquery_helper',
-      import: path.join(__dirname, 'src/js', 'jquery.js'),
+      import: path.join(__dirname, 'frontend/js', 'jquery.js'),
       filename: '[name].[contenthash].jquery.js',
     },
     jquery_helper: {
       dependOn: 'main',
-      import: path.join(__dirname, 'src/js', 'jquery_helper.js'),
+      import: path.join(__dirname, 'frontend/js', 'jquery_helper.js'),
       filename: '[name].[contenthash].jquery_helper.js',
     },
   },
@@ -30,7 +30,7 @@ module.exports = {
     publicPath: '/',
     path: path.join(__dirname, 'dist'),
     filename: 'index.[contenthash:5].js',
-    assetModuleFilename: 'src/assets/images/[name].[ext]'
+    assetModuleFilename: 'frontend/assets/images/[name].[ext]'
   },
   module: {
     rules: [
@@ -47,34 +47,34 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: base_template,
       filename: 'index.html',
-      content: fs.readFileSync(path.join(__dirname, 'src/includes', 'home.html')),
+      content: fs.readFileSync(path.join(__dirname, 'frontend/includes', 'home.html')),
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/includes', 'choice.html'),
+      template: path.join(__dirname, 'frontend/includes', 'choice.html'),
       filename: 'choice.html',
       chunks: [],
       inject: false
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/includes', 'faq.html'),
+      template: path.join(__dirname, 'frontend/includes', 'faq.html'),
       filename: 'faq.html',
       chunks: [],
       inject: false
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/includes', 'home.html'),
+      template: path.join(__dirname, 'frontend/includes', 'home.html'),
       filename: 'home.html',
       chunks: [],
       inject: false
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/includes', 'test.html'),
+      template: path.join(__dirname, 'frontend/includes', 'test.html'),
       filename: 'test.html',
       chunks: [],
       inject: false
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/includes', 'result.html'),
+      template: path.join(__dirname, 'frontend/includes', 'result.html'),
       filename: 'result.html',
       chunks: [],
       inject: false
@@ -89,7 +89,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
     }),
-    new FaviconsWebpackPlugin(path.join('src/assets/img', 'favicon.png')),
+    new FaviconsWebpackPlugin(path.join('frontend/assets/img', 'favicon.png')),
     // Подключение jQuery как глобальной переменной
     new webpack.ProvidePlugin({
       $: 'jquery',
@@ -98,7 +98,7 @@ module.exports = {
     })
   ],
   devServer: {
-    watchFiles: path.join(__dirname, 'src'),
+    watchFiles: path.join(__dirname, 'frontend'),
     port: 9090,
   },
 }
