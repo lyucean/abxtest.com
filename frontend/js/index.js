22162a8656f9caf3e4f986e89d36621dd7073577
lyucean <lyucean@gmail.com>
2024-09-16 14:26:59 +0300

    + sentry для js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,20 @@
 
 import '../css/bootstrap.css';
 import '../css/style.css';
+
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+    dsn: "https://82495f8ea55a1fdac19bb8bd074c1169@o443919.ingest.us.sentry.io/4507962216939520",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
